dadabce1f8f7331e9b0fc8399ead0b233f705aeb
ispirett <ispirett@gmail.com>
2019-11-27 19:12:56 -0400

    featureh(stickler): add stickler

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>React Bookstore</h1>
+       <h1>React Bookstore real</h1>
       </header>
     </div>
   );
